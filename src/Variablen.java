a0edb2e94765c80de2864d2358a39b86c1cfb5fa
Juliane Bonenkamp <JBON182@more2learn.de>
2025-02-17 11:57:46 +0100

    Initial commit for Eclipse Java projekt

---
+++ src/Variablen.java
@@ -0,0 +1,45 @@
+
+public class Variablen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 8 primitive Datentypen in Java
+		/*
+		 * byte
+		 * short
+		 * int
+		 * long
+		 * float
+		 * double
+		 * boolean
+		 * char
+		 */
+		//Dekleration
+		//<Datentyp><Variablenname>=<Wert>;
+		
+		int varZahl1 = 3;
+		long varZahl2 = 333;
+		
+		double varZahl3 = 322.23;
+		float vatZahl4 = 32.223f;
+		
+		boolean userklick = false;
+		
+		char zeichen = '!';
+		
+		varZahl1 = 5;
+		
+		//Inizialisierung
+		int highscore = 0;
+		System.out.println("Dein Highscore ist: "+ highscore);
+		//Zuweisung
+		highscore = 10;
+		System.out.println("Dein Highscore ist: "+ highscore);
+		highscore = 20;
+		System.out.println("Dein Highscore ist: "+ highscore);
+		
+				
+
+	}
+
+}
