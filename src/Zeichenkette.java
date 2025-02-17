ad9d0b60934795b4029f015f16f67e6d8ebe32e7
Juliane Bonenkamp <JBON182@more2learn.de>
2025-02-17 12:39:28 +0100

    Kursbeginn Variabelen Typen

---
+++ src/Zeichenkette.java
@@ -0,0 +1,31 @@
+
+public class Zeichenkette {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 8 primitive Datentypen in Java
+				/*
+				 * byte
+				 * short
+				 * int
+				 * long
+				 * float
+				 * double
+				 * boolean
+				 * char
+				 */
+			
+		//Komplexedatentypen bestehen aus vielen einzelteilen
+		//z.B. besteht String aus mehreren char
+		//
+
+		char varZeichen = '!';
+		String varZeichenkette = "ich bin eine Zeichenkette";
+		
+		String name = "Juliane";
+		String alter= "55";
+		
+		System.out.println("Dein Name ist " + name + " dein Alter ist " + alter);
+	}
+
+}
---
+++ src/ZeichneBool.java
@@ -0,0 +1,27 @@
+
+public class ZeichneBool {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 8 primitive Datentypen in Java
+		/*
+		 * byte
+		 * short
+		 * int
+		 * long
+		 * float
+		 * double
+		 * boolean (Aussprache: bool)
+		 * char (von charakter = Zeichen)
+		 */
+		//Komplexedatentypen
+		//String (Zeicehnketten)
+		
+		//<Datentyp><Variablenname> = <Wert> ;
+		
+		char varZeichen = '\u00A9'; // Unicode 
+		System.out.println("Das Bild unterlieget dem " + varZeichen);
+		
+	}
+
+}
